a6e4917255077d280caa1fc6071a2a74bcb00f7e
Harlan Wilton <harlan@harlanzw.com>
2022-11-08 14:05:47 +1100

    chore: drop first-party nuxt

--- packages/nuxt/src/runtime/plugin.ts
+++
@@ -1,9 +0,0 @@
-import { createHead } from '@unhead/vue'
-import { defineNuxtPlugin } from '#app'
-
-// Note: This should always be a partial match to nuxt's internal vueuse-head plugin
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const head = createHead()
-  head.install(nuxtApp.vueApp)
-})
