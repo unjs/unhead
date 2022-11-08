4120799450c9e65a3cbed57d0d630f62838f98cf
Harlan Wilton <harlan@harlanzw.com>
2022-11-08 14:06:19 +1100

    fix(build): inline zhead

--- packages/schema/build.config.ts
+++ packages/schema/build.config.ts
@@ -3,9 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   clean: true,
   declaration: true,
-  rollup: {
-    inlineDependencies: true,
-  },
   entries: [
     { input: 'src/index', name: 'index' },
   ],
--- packages/ssr/build.config.ts
+++ packages/ssr/build.config.ts
@@ -3,6 +3,9 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   clean: true,
   declaration: true,
+  rollup: {
+    inlineDependencies: true,
+  },
   entries: [
     { input: 'src/index', name: 'index' },
   ],
