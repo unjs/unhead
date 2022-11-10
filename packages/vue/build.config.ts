f8cd7b9c1761f771a264eb503e5f977afc5402fe
Harlan Wilton <harlan@harlanzw.com>
2022-11-10 17:54:07 +1100

    chore(pkg): drop extra vue builds

--- packages/vue/build.config.ts
+++ packages/vue/build.config.ts
@@ -3,7 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   clean: true,
   declaration: true,
-  failOnWarn: false,
   rollup: {
     inlineDependencies: true,
     emitCJS: true,
@@ -11,8 +10,5 @@ export default defineBuildConfig({
   externals: ['vue'],
   entries: [
     { input: 'src/index', name: 'index' },
-    { builder: 'mkdist', input: 'src/runtime/alias', outDir: 'dist/runtime/alias' },
-    { builder: 'mkdist', input: 'src/runtime/server', outDir: 'dist/runtime/server' },
-    { builder: 'mkdist', input: 'src/runtime/client', outDir: 'dist/runtimes/client' },
   ],
 })
