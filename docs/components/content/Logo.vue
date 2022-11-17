c1e9373ed7c27e1533020aeae16f0df81a860172
Harlan Wilton <harlan@harlanzw.com>
2022-11-18 03:41:05 +1100

    doc: prefer @unjs

--- docs/components/content/Logo.vue
+++ docs/components/content/Logo.vue
@@ -5,7 +5,7 @@
       <icon name="lucide:triangle" class="text-black w-5 h-5 mr-2" />
     </div>
     <div class="flex flex-col">
-      <span class="opacity-60 text-xs">@harlan-zw/</span>
+      <span class="opacity-60 text-xs">@unjs/</span>
       <h1 class="font-bold text-xl">unhead</h1>
     </div>
   </div>
