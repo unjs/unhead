d50ac9439fcce74198e494e0ddf4f13dbdc4b209
Harlan Wilton <harlan@harlanzw.com>
2022-12-08 15:22:55 +1100

    fix(InferSeoMetaPlugin): avoid duplicate tags

--- packages/addons/src/plugins/inferSeoMetaPlugin.ts
+++ packages/addons/src/plugins/inferSeoMetaPlugin.ts
@@ -1,4 +1,5 @@
 import { defineHeadPlugin } from 'unhead'
+import {Head, HeadEntry} from "@unhead/schema";
 
 export interface InferSeoMetaPluginOptions {
   /**
@@ -29,58 +30,50 @@ export interface InferSeoMetaPluginOptions {
 
 export const InferSeoMetaPlugin = (options?: InferSeoMetaPluginOptions) => defineHeadPlugin({
   hooks: {
-    tags: {
-      resolve(ctx) {
-        const tags = ctx.tags.reverse()
-        const title = tags.find(t => t.tag === 'title' && !!t.children)
-        const ogTitle = tags.find(t => t.tag === 'meta' && t.props.property === 'og:title' && t._e === title?._e)
-        if (title && !ogTitle) {
-          ctx.tags.push({
-            // note: we need to add this new tag to the useHead entry of title
-            // (if title is removed, we remove this tag too)
-            _e: title._e,
-            tag: 'meta',
-            props: {
+    entries: {
+      resolve({ entries }) {
+        for(const entry of entries) {
+          const inputKey = entry.resolvedInput ? 'resolvedInput' : 'input'
+          const input = entry[inputKey]
+          const resolvedMeta : Required<Head>['meta'] = input.meta || []
+          const title = input.title
+          const ogTitle = resolvedMeta.find(meta => meta.property === 'og:title')
+          const description = resolvedMeta.find(meta => meta.name === 'description')?.content
+          const ogDescription = resolvedMeta.find(meta => meta.property === 'og:description')
+
+          if (title && !ogTitle) {
+            entry[inputKey].meta.push({
               property: 'og:title',
-              content: options?.ogTitle ? options.ogTitle(title.children!) : title.children,
-            },
-          })
+              content: options?.ogTitle ? options.ogTitle(title) : title,
+            })
+          }
+          if (description && !ogDescription) {
+            entry[inputKey].meta.push({
+              property: 'og:description',
+              content: options?.ogDescription ? options.ogDescription(String(description)) : description,
+            })
+          }
         }
-        const description = ctx.tags.find(t => t.tag === 'meta' && t.props.name === 'description' && !!t.props.content)
-        const ogDescription = tags.find(t => t.tag === 'meta' && t.props.property === 'og:description' && t._e === description?._e)
-        if (description && !ogDescription) {
-          ctx.tags.push({
-            _e: description._e,
-            tag: 'meta',
-            props: {
-              name: 'og:description',
-              content: options?.ogDescription ? options.ogDescription(description.props.content) : description.props.content,
-            },
-          })
+        const rootEntry: HeadEntry<Head> = {
+          _i: -1,
+          _sde: {},
+          input: {
+            meta: [
+              {
+                property: 'twitter:card',
+                content: options?.twitterCard || 'summary_large_image',
+                tagPriority: 'low',
+              },
+              {
+                name: 'robots',
+                content: 'max-snippet: -1; max-image-preview: large; max-video-preview: -1',
+                tagPriority: 'low',
+              }
+            ]
+          }
         }
-        // if we have an image, add twitter:card if missing
-        const ogImage = ctx.tags.find(t => t.tag === 'meta' && t.props.property === 'og:image')
-        if (ogImage && !ctx.tags.find(t => t.tag === 'meta' && t.props.property === 'twitter:card')) {
-          ctx.tags.push({
-            _e: ogImage._e,
-            tag: 'meta',
-            props: {
-              property: 'twitter:card',
-              content: options?.twitterCard || 'summary_large_image',
-            },
-          })
-        }
-        if (options?.robots !== false && !ctx.tags.find(t => t.tag === 'meta' && t.props.name === 'robots')) {
-          ctx.tags.push({
-            _e: ctx.tags[0]._e,
-            tag: 'meta',
-            props: {
-              name: 'robots',
-              content: 'max-snippet: -1; max-image-preview: large; max-video-preview: -1',
-            },
-          })
-        }
-      },
+        entries.unshift(rootEntry)
+      }
     },
   },
 })
--- test/addons/infer-seo-meta.test.ts
+++ test/addons/infer-seo-meta.test.ts
@@ -1,93 +1,75 @@
 import { describe, it } from 'vitest'
 import { InferSeoMetaPlugin } from '../../packages/addons/src'
+import {createHead} from "unhead";
+import {renderSSRHead} from "../../packages/ssr";
 
-describe('state', () => {
-  it('exists', async () => {
-    const ctx = {
-      tags: [
+describe('InferSeoMetaPlugin', () => {
+  it('simple', async () => {
+    const head = createHead({
+      plugins: [InferSeoMetaPlugin()]
+    })
+
+    head.push({
+      title: 'My Title',
+      meta: [
         {
-          _e: 1,
-          children: 'test',
-          props: {},
-          tag: 'title',
+          name: 'description',
+          content: 'My Description',
         },
         {
-          _e: 1,
-          props: {
-            content: 'test',
-            name: 'description',
-          },
-          tag: 'meta',
-        },
-        {
-          _e: 1,
-          props: {
-            property: 'og:image',
-            name: 'https://example.com/image.jpg',
-          },
-          tag: 'meta',
-        },
-      ],
-    }
-    // @ts-expect-error hacky run of hook
-    InferSeoMetaPlugin().hooks.tags.resolve(ctx)
-    expect(ctx.tags).toMatchInlineSnapshot(`
-      [
-        {
-          "_e": 1,
-          "props": {
-            "name": "https://example.com/image.jpg",
-            "property": "og:image",
-          },
-          "tag": "meta",
-        },
-        {
-          "_e": 1,
-          "props": {
-            "content": "test",
-            "name": "description",
-          },
-          "tag": "meta",
-        },
-        {
-          "_e": 1,
-          "children": "test",
-          "props": {},
-          "tag": "title",
-        },
-        {
-          "_e": 1,
-          "props": {
-            "content": "test",
-            "property": "og:title",
-          },
-          "tag": "meta",
-        },
-        {
-          "_e": 1,
-          "props": {
-            "content": "test",
-            "name": "og:description",
-          },
-          "tag": "meta",
-        },
-        {
-          "_e": 1,
-          "props": {
-            "content": "summary_large_image",
-            "property": "twitter:card",
-          },
-          "tag": "meta",
-        },
-        {
-          "_e": 1,
-          "props": {
-            "content": "max-snippet: -1; max-image-preview: large; max-video-preview: -1",
-            "name": "robots",
-          },
-          "tag": "meta",
+          property: 'og:image',
+          content: 'https://example.com/image.jpg',
         },
       ]
+    })
+
+    expect(await renderSSRHead(head)).toMatchInlineSnapshot(`
+      {
+        "bodyAttrs": "",
+        "bodyTags": "",
+        "bodyTagsOpen": "",
+        "headTags": "<title>My Title</title>
+      <meta name=\\"description\\" content=\\"My Description\\">
+      <meta property=\\"og:image\\" content=\\"https://example.com/image.jpg\\">
+      <meta property=\\"og:title\\" content=\\"My Title\\">
+      <meta property=\\"og:description\\" content=\\"My Description\\">
+      <meta property=\\"twitter:card\\" content=\\"summary_large_image\\">
+      <meta name=\\"robots\\" content=\\"max-snippet: -1; max-image-preview: large; max-video-preview: -1\\">",
+        "htmlAttrs": "",
+      }
+    `)
+  })
+  it('conflicts', async () => {
+    const head = createHead({
+      plugins: [InferSeoMetaPlugin()]
+    })
+
+    head.push({
+      title: 'Title',
+      meta: [
+        {
+          name: 'og:description',
+          content: 'My OG description',
+        },
+        {
+          property: 'og:title',
+          content: 'My OG title',
+        },
+      ]
+    })
+
+    expect(await renderSSRHead(head)).toMatchInlineSnapshot(`
+      {
+        "bodyAttrs": "",
+        "bodyTags": "",
+        "bodyTagsOpen": "",
+        "headTags": "<title>Title</title>
+      <meta name=\\"og:description\\" content=\\"My OG description\\">
+      <meta property=\\"og:title\\" content=\\"My OG title\\">
+      <meta property=\\"twitter:card\\" content=\\"summary_large_image\\">
+      <meta name=\\"robots\\" content=\\"max-snippet: -1; max-image-preview: large; max-video-preview: -1\\">",
+        "htmlAttrs": "",
+      }
     `)
   })
 })
