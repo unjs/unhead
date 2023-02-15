87ed11ae695131ae126c0b92ee590db19ef55e8c
LittleSound <464388324@qq.com>
2023-02-15 17:37:28 +0800

    chore: add unit test

--- test/vue/dom/keepalive.test.ts
+++ test/vue/dom/keepalive.test.ts
@@ -2,8 +2,8 @@
 
 import { describe, it } from 'vitest'
 import { createHead, injectHead, useHead } from '@unhead/vue'
-import { nextTick, ref } from 'vue'
-import { useKeepAliveSetup } from '../util'
+import { defineComponent, h, KeepAlive, nextTick, ref } from 'vue'
+import { mount, useKeepAliveSetup } from '../util'
 
 describe('keepalive', () => {
   it('basic', async () => {
@@ -71,6 +71,112 @@ describe('keepalive', () => {
           "_e": 0,
           "_p": 0,
           "children": "hello",
+          "props": {},
+          "tag": "title",
+        },
+      ]
+    `)
+  })
+
+  it('keepalive component change', async () => {
+    const Comp1 = defineComponent({
+      setup() {
+        useHead({ title: 'home' })
+      }
+    })
+
+    const Comp2 = defineComponent({
+      setup() {
+        useHead({ title: 'about' })
+      }
+    })
+
+    const Provider = defineComponent({
+      components: { Comp1 },
+      setup() {
+        const head = ref(injectHead())
+        const name = ref('home')
+        return { name, head }
+      },
+      render() {
+        return h('div', [h(KeepAlive, [this.name === 'home' ? h(Comp1, { ref: 'comp1' }) : h(Comp2, { ref: 'comp2' })])])
+      },
+    })
+
+    const app = mount(Provider, () => ({ head: createHead() }))
+
+    // 1
+    expect(await app.head.resolveTags()).toMatchInlineSnapshot(`
+      [
+        {
+          "_d": "title",
+          "_e": 0,
+          "_p": 0,
+          "children": "home",
+          "props": {},
+          "tag": "title",
+        },
+      ]
+    `)
+
+    // 2
+    app.name = 'about'
+    await nextTick()
+    expect(await app.head.resolveTags()).toMatchInlineSnapshot(`
+      [
+        {
+          "_d": "title",
+          "_e": 1,
+          "_p": 1024,
+          "children": "about",
+          "props": {},
+          "tag": "title",
+        },
+      ]
+    `)
+
+    // 3
+    app.name = 'home'
+    await nextTick()
+    expect(await app.head.resolveTags()).toMatchInlineSnapshot(`
+      [
+        {
+          "_d": "title",
+          "_e": 0,
+          "_p": 0,
+          "children": "home",
+          "props": {},
+          "tag": "title",
+        },
+      ]
+    `)
+
+    // 4
+    app.name = 'about'
+    await nextTick()
+    expect(await app.head.resolveTags()).toMatchInlineSnapshot(`
+      [
+        {
+          "_d": "title",
+          "_e": 1,
+          "_p": 1024,
+          "children": "about",
+          "props": {},
+          "tag": "title",
+        },
+      ]
+    `)
+
+    // 5
+    app.name = 'home'
+    await nextTick()
+    expect(await app.head.resolveTags()).toMatchInlineSnapshot(`
+      [
+        {
+          "_d": "title",
+          "_e": 0,
+          "_p": 0,
+          "children": "home",
           "props": {},
           "tag": "title",
         },
