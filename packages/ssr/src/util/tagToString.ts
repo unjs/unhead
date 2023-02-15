482f37c1a515aaed5940b4f4c18b7725cd299598
Harlan Wilton <harlan@harlanzw.com>
2023-02-16 03:20:44 +1100

    fix: title XSS issue vulnerability

--- packages/ssr/src/util/tagToString.ts
+++ packages/ssr/src/util/tagToString.ts
@@ -1,9 +1,21 @@
 import type { HeadTag } from '@unhead/schema'
-import { SelfClosingTags } from '@unhead/shared'
-import { propsToString } from '.'
+import { SelfClosingTags, TagsWithInnerContent } from '@unhead/shared'
+import { propsToString } from './propsToString'
+
+export function encodeHtmlEntities(str: string) {
+  return str.replace(/[\u00A0-\u9999<>\&]/gim, i => `&#${i.charCodeAt(0)};`)
+}
 
 export const tagToString = <T extends HeadTag>(tag: T) => {
   const attrs = propsToString(tag.props)
   const openTag = `<${tag.tag}${attrs}>`
-  return SelfClosingTags.includes(tag.tag) ? openTag : `${openTag}${tag.children || ''}</${tag.tag}>`
+  // get the encoding depending on the tag type
+  if (!TagsWithInnerContent.includes(tag.tag))
+    return SelfClosingTags.includes(tag.tag) ? openTag : `${openTag}</${tag.tag}>`
+
+  let content = tag.children || ''
+  if (content && tag.tag === 'title')
+    // content needs to be encoded to avoid XSS, only for title
+    content = encodeHtmlEntities(content)
+  return SelfClosingTags.includes(tag.tag) ? openTag : `${openTag}${content}</${tag.tag}>`
 }
--- test/unhead/ssr/xss.test.ts
+++ test/unhead/ssr/xss.test.ts
@@ -10,13 +10,23 @@ describe('xss', () => {
       title: '</title><script>alert(1)</script>',
     })
 
+
+    head.push({
+      style: [
+        {
+          children: '</style><script>alert(1)</script>',
+        }
+      ]
+    })
+
     const ctx = await renderSSRHead(head)
     expect(ctx).toMatchInlineSnapshot(`
       {
         "bodyAttrs": "",
         "bodyTags": "",
         "bodyTagsOpen": "",
-        "headTags": "<title>&#60;/title&#62;&#60;script&#62;alert(1)&#60;/script&#62;</title>",
+        "headTags": "<title>&#60;/title&#62;&#60;script&#62;alert(1)&#60;/script&#62;</title>
+      <style></style><script>alert(1)</script></style>",
         "htmlAttrs": "",
       }
     `)
