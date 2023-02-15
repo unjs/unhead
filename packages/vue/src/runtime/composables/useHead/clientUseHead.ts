e62dcb1614b947296934189114b0e37b9ac0a733
LittleSound <464388324@qq.com>
2023-02-15 17:51:31 +0800

    fix: keepalive bug

--- packages/vue/src/runtime/composables/useHead/clientUseHead.ts
+++ packages/vue/src/runtime/composables/useHead/clientUseHead.ts
@@ -10,11 +10,12 @@ export function clientUseHead<T extends MergeHead>(input: UseHeadInput<T>, optio
 
   const resolvedInput: Ref<ReactiveHead> = ref({})
   watchEffect(() => {
-    resolvedInput.value = resolveUnrefHeadInput(input)
+    resolvedInput.value = deactivated.value
+      ? {}
+      : resolveUnrefHeadInput(input)
   })
   const entry: ActiveHeadEntry<UseHeadInput<T>> = head.push(resolvedInput.value, options)
-  watch([resolvedInput, deactivated], ([e, disable]) => {
-    if (!disable)
+  watch(resolvedInput, (e) => {
       entry.patch(e)
   })
 
