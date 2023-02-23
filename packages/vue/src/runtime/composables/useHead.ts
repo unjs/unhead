4f604f918bdf7743c60f456a80dd69e9c63e1a2e
Harlan Wilton <harlan@harlanzw.com>
2023-02-23 16:30:54 +1100

    fix: deprecate verbose composables

---
+++ packages/vue/src/runtime/composables/useHead.ts
@@ -0,0 +1,16 @@
+import type { ActiveHeadEntry, HeadEntryOptions, MergeHead } from '@unhead/schema'
+import type { UseHeadInput } from '@unhead/vue'
+import { injectHead } from '@unhead/vue'
+import { IsBrowser } from '../../env'
+import { clientUseHead as _clientUseHead } from './useHead/clientUseHead'
+import { serverUseHead as _serverUseHead } from './useHead/serverUseHead'
+
+export function useHead<T extends MergeHead>(input: UseHeadInput<T>, options: HeadEntryOptions = {}): ActiveHeadEntry<UseHeadInput<T>> | void {
+  const head = injectHead()
+  if (head) {
+    const isBrowser = IsBrowser || !!head.resolvedOptions?.document
+    if ((options.mode === 'server' && isBrowser) || (options.mode === 'client' && !isBrowser))
+      return
+    return isBrowser ? _clientUseHead(input, options) : _serverUseHead(input, options)
+  }
+}
