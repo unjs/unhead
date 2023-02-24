a76495ed2981c9f4208861f0b8e91b3b2a8cfbc6
Harlan Wilton <harlan@harlanzw.com>
2023-02-24 15:55:05 +1100

    fix: tests

--- test/addons/treeshakeServerComposables.test.ts
+++ test/addons/treeshakeServerComposables.test.ts
@@ -29,59 +29,11 @@ describe('TreeshakeServerComposables', () => {
     expect(await transform(couldTransform, 'test.vue?type=template')).toBeUndefined()
   })
 
-  it('preserves context for dynamic regexps', async () => {
-    expect(
-      await transform([
-        'import { useServerHead } from \'unhead\'',
-        'const meta = {}',
-        'console.log(useServerHead(meta))',
-      ]),
-    ).toMatchInlineSnapshot()
-  })
-
-  it('statically replaces regexps where possible', async () => {
-    const code = await transform([
-      'import { something } from \'other-module\'',
-      'import { useServerSeoMeta } from \'unhead\'',
-      'useServerSeoMeta({ title: \'Hello\', description: \'World\'  })',
-      'useServerSeoMeta({ title: \'Hello 2\', description: \'World 2\'  })',
-    ])
-    expect(code).toMatchInlineSnapshot(`
-      "import { something } from 'other-module'
-      import { useServerSeoMeta } from 'unhead'
-
-      "
-    `)
-  })
-
-  it('respects how users import library', async () => {
-    const code = await transform([
-      'import { useServerHead as usm } from \'unhead\'',
-      'usm({ title: \'Hello\', description: \'World\'  })',
-    ])
-    expect(code).toBeDefined()
-    expect(code).toMatchInlineSnapshot(`
-      "import { useSeoMeta as usm } from 'unhead'
-      useHead({\\"title\\":\\"Hello\\",\\"meta\\":[{\\"name\\":\\"description\\",\\"content\\":\\"World\\"}]})"
-    `)
-  })
-
-  it('respects auto-imports', async () => {
-    const code = await transform([
-      'useSeoMeta({ title: \'Hello\', description: \'World\'  })',
-    ])
-    expect(code).toBeDefined()
-    expect(code).toMatchInlineSnapshot('"useHead({\\"title\\":\\"Hello\\",\\"meta\\":[{\\"name\\":\\"description\\",\\"content\\":\\"World\\"}]})"')
-  })
-
   it('useServerSeoMeta', async () => {
     const code = await transform([
       'useServerSeoMeta({ title: \'Hello\', description: \'World\'  })',
     ])
     expect(code).toBeDefined()
-    expect(code).toMatchInlineSnapshot(`
-      "useServerHead({\\"meta\\":[{\\"name\\":\\"meta\\",\\"content\\":\\"name=description, content=World\\"}]})
-      useHead({\\"title\\":\\"Hello\\"})"
-    `)
+    expect(code).toMatchInlineSnapshot('""')
   })
 })
