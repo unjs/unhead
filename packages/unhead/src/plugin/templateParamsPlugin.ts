0b131bb65193868c669aadca8328e927b810d1ca
Harlan Wilton <harlan@harlanzw.com>
2023-02-26 14:03:34 +1100

    fix(templateParams): always process params for %s usage

--- packages/unhead/src/plugin/templateParamsPlugin.ts
+++ packages/unhead/src/plugin/templateParamsPlugin.ts
@@ -42,19 +42,18 @@ export function TemplateParamsPlugin() {
         const { tags } = ctx
         // find templateParams
         const title = tags.find(tag => tag.tag === 'title')?.textContent
-        const templateParamsIdx = tags.findIndex(tag => tag.tag === 'templateParams')
-        if (templateParamsIdx !== -1) {
-          const templateParams = tags[templateParamsIdx].textContent as unknown as TemplateParams
-          templateParams.pageTitle = templateParams.pageTitle || title || ''
-          delete tags[templateParamsIdx]
-          for (const tag of tags.filter(Boolean)) {
-            if (['titleTemplate', 'title'].includes(tag.tag) && typeof tag.textContent === 'string')
-              tag.textContent = processTemplateParams(tag.textContent, templateParams)
-            else if (tag.tag === 'meta' && typeof tag.props.content === 'string')
-              tag.props.content = processTemplateParams(tag.props.content, templateParams)
-            else if (tag.tag === 'script' && ['application/json', 'application/ld+json'].includes(tag.props.type) && typeof tag.innerHTML === 'string')
-              tag.innerHTML = processTemplateParams(tag.innerHTML, templateParams)
-          }
+        const idx = tags.findIndex(tag => tag.tag === 'templateParams')
+        // we always process params so we can substitute the title
+        const params = idx !== -1 ? tags[idx].textContent as unknown as TemplateParams : {}
+        params.pageTitle = params.pageTitle || title || ''
+        delete tags[idx]
+        for (const tag of tags.filter(Boolean)) {
+          if (['titleTemplate', 'title'].includes(tag.tag) && typeof tag.textContent === 'string')
+            tag.textContent = processTemplateParams(tag.textContent, params)
+          else if (tag.tag === 'meta' && typeof tag.props.content === 'string')
+            tag.props.content = processTemplateParams(tag.props.content, params)
+          else if (tag.tag === 'script' && ['application/json', 'application/ld+json'].includes(tag.props.type) && typeof tag.innerHTML === 'string')
+            tag.innerHTML = processTemplateParams(tag.innerHTML, params)
         }
         ctx.tags = tags.filter(Boolean)
       },
