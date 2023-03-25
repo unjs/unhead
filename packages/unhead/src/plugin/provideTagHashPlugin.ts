849febfd6261556b29fcdb4af1edac4bf20914fc
Harlan Wilton <harlan@harlanzw.com>
2023-03-25 12:44:39 +1100

    refactor: clean up hashing plugin

--- packages/unhead/src/plugin/provideTagHashPlugin.ts
+++ packages/unhead/src/plugin/provideTagHashPlugin.ts
@@ -1,32 +1,23 @@
-import { defineHeadPlugin, hashCode, hashTag } from '@unhead/shared'
-import { getActiveHead } from '..'
+import {defineHeadPlugin, hashCode, hashTag} from '@unhead/shared'
+
+const DupeableTags = ['link', 'style', 'script', 'noscript']
 
 export const ProvideTagHashPlugin = () => {
   return defineHeadPlugin({
     hooks: {
-      'tag:normalise': (ctx) => {
-        const { tag, resolvedOptions } = ctx
-
+      'tag:normalise': ({ tag, resolvedOptions }) => {
         if (resolvedOptions.experimentalHashHydration === true) {
           // always generate a hash
           tag._h = hashTag(tag)
         }
 
+        // only if the user has provided a key
         // only tags which can't dedupe themselves, ssr only
-        if (![
-          'link',
-          'style',
-          'script',
-          'noscript',
-        ].includes(tag.tag) || !tag.key || getActiveHead()?.resolvedOptions?.document)
-          return
-
-        // when we are SSR rendering tags which are server only and have a dedupe key, we need to provide a hash
-        // client side should not be here if the entry is server mode (entry should be ignored)
-        // if a user provides a key we will also add the hash as a way to ensure hydration works, good for
-        // when SSR / CSR does not match
-        tag._h = hashCode(tag.key)
-        tag.props[`data-h-${tag._h}`] = ''
+        if (tag.key && DupeableTags.includes(tag.tag)) {
+          // add a HTML key so the client-side can hydrate without causing duplicates
+          tag._h = hashCode(tag.key)
+          tag.props[`data-h-${tag._h}`] = ''
+        }
       },
     },
   })
