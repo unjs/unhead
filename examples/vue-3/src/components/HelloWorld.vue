eb0d0c09dd20ed435c7c33e39eaeede1d0fb276a
t0yohei <30691457+t0yohei@users.noreply.github.com>
2023-06-26 12:59:24 +0900

    chore: add Vue3 example (#160)
    Co-authored-by: Harlan Wilton <harlan@harlanzw.com>

---
+++ examples/vue-3/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue'
+
+defineProps<{
+  msg: string
+}>()
+
+useHead({
+  title: 'Vue 3 updated',
+  meta: [
+    {
+      name: 'description',
+      content: 'Vue 3 Head is a Vue 3 composition API for managing the document head.',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: 'https://vueuse.org/logo.png',
+    },
+  ],
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">
+      {{ msg }}
+    </h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
