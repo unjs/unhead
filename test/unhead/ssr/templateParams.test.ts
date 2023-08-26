1ec6b2c90351889003eb0e4a07cc5999b2497694
Harlan Wilton <harlan@harlanzw.com>
2023-08-26 19:54:57 +0200

    chore: fix payload test snapshot

--- test/unhead/ssr/templateParams.test.ts
+++ test/unhead/ssr/templateParams.test.ts
@@ -67,7 +67,7 @@ describe('ssr templateParams', () => {
 
     expect(headTags).toMatchInlineSnapshot(`
       "<title>My Awesome Site</title>
-      <script id=\\"unhead:payload\\">{\\"titleTemplate\\":\\"%s %separator %siteName\\",\\"templateParams\\":{\\"separator\\":\\"|\\",\\"siteName\\":\\"My Awesome Site\\"}}</script>"
+      <script id=\\"unhead:payload\\" type=\\"application/json\\">{\\"titleTemplate\\":\\" | My Awesome Site\\",\\"templateParams\\":{\\"separator\\":\\"|\\",\\"siteName\\":\\"My Awesome Site\\"}}</script>"
     `)
   })
 })
