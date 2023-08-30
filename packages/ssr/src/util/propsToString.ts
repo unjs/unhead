5d45d7b9be5b058eea8b2ec52f86dc2024b83259
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-30 19:18:28 +0200

    feat(util): do not exclude value `true` for data properties

--- packages/ssr/src/util/propsToString.ts
+++ packages/ssr/src/util/propsToString.ts
@@ -7,7 +7,7 @@ export function propsToString(props: Record<string, any>) {
 
     let attribute = key
 
-    if (value !== true && value !== '')
+    if ((key.startsWith('data-') || value !== true) && value !== '')
       attribute += `="${String(value).replace(/"/g, '&quot;')}"`
 
     handledAttributes.push(attribute)
--- test/ssr/util.test.ts
+++ test/ssr/util.test.ts
@@ -11,21 +11,22 @@ describe('propsToString', () => {
 
   it('stringifies all properties correctly', async () => {
     expect(propsToString({
-      array: ['a', 1],
-      big: 100n,
-      bigInt: BigInt(1),
-      binary: 0b1010,
-      false: false,
-      hex: 0xF00D,
-      null: null,
-      number: 1337,
-      object: { a: 1 },
-      octal: 0o744,
-      string: 'string',
-      stringEmpty: '',
-      symbol: Symbol('a'),
-      true: true,
+      'array': ['a', 1],
+      'big-int': BigInt(1),
+      'big': 100n,
+      'binary': 0b1010,
+      'boolean-false': false,
+      'boolean-true': true,
+      'data-foo': true,
+      'hex': 0xF00D,
+      'null': null,
+      'number': 1337,
+      'object': { a: 1 },
+      'octal': 0o744,
+      'string-empty': '',
+      'string': 'string',
+      'symbol': Symbol('a'),
       undefined,
-    })).toStrictEqual(' array="a,1" big="100" bigInt="1" binary="10" hex="61453" number="1337" object="[object Object]" octal="484" string="string" stringEmpty symbol="Symbol(a)" true')
+    })).toStrictEqual(' array="a,1" big-int="1" big="100" binary="10" boolean-true data-foo="true" hex="61453" number="1337" object="[object Object]" octal="484" string-empty string="string" symbol="Symbol(a)"')
   })
 })
