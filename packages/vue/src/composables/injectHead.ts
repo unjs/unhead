c4644dcb7a78692fa26ce2af6ed4794d099b4c43
Harlan Wilton <harlan@harlanzw.com>
2023-09-03 16:06:46 +0200

    fix(vue): unsafely inject head
    Fixes #209

--- packages/vue/src/composables/injectHead.ts
+++ packages/vue/src/composables/injectHead.ts
@@ -1,9 +1,9 @@
 import type { MergeHead } from '@unhead/schema'
-import { hasInjectionContext, inject } from 'vue'
+import { inject } from 'vue'
 import { getActiveHead } from 'unhead'
 import type { VueHeadClient } from '../types'
 import { headSymbol } from '../createHead'
 
 export function injectHead<T extends MergeHead>() {
-  return ((hasInjectionContext() && inject(headSymbol)) || getActiveHead()) as VueHeadClient<T>
+  return (inject(headSymbol) || getActiveHead()) as VueHeadClient<T>
 }
