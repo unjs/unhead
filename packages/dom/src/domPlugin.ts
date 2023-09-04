201a1c1db085fbb3f74d0ebfbbf12792e021453c
Harlan Wilton <harlan@harlanzw.com>
2023-09-05 01:15:01 +0200

    fix(dom): hydrate initial payload with current title

--- packages/dom/src/domPlugin.ts
+++ packages/dom/src/domPlugin.ts
@@ -10,7 +10,10 @@ export interface DomPluginOptions extends RenderDomHeadOptions {
   return defineHeadPlugin((head) => {
     // restore initial entry from payload (titleTemplate and templateParams)
     const initialPayload = head.resolvedOptions.document?.head.querySelector('script[id="unhead:payload"]')?.innerHTML || false
-    initialPayload && head.push(JSON.parse(initialPayload))
+    initialPayload && head.push({
+      title: document.title,
+      ...JSON.parse(initialPayload),
+    })
     return {
       mode: 'client',
       hooks: {
