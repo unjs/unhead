ef87a3b5252a092ab7bfe0050c649034845cbac1
Harlan Wilton <harlan@harlanzw.com>
2023-09-10 12:16:11 +0200

    refactor: smaller payload code

--- packages/unhead/src/plugins/payload.ts
+++ packages/unhead/src/plugins/payload.ts
@@ -4,15 +4,15 @@ export default defineHeadPlugin({
   mode: 'server',
   hooks: {
     'tags:resolve': function (ctx) {
-      const csrPayload: Record<string, any> = {}
+      const payload: { titleTemplate: string | ((s: string) => string); templateParams: Record<string, string>; title: string } = {}
       ctx.tags.filter(tag => ['titleTemplate', 'templateParams', 'title'].includes(tag.tag) && tag._m === 'server')
         .forEach((tag) => {
-          csrPayload[tag.tag] = tag.tag.startsWith('title') ? tag.textContent : tag.props
+          payload[tag.tag] = tag.tag.startsWith('title') ? tag.textContent : tag.props
         })
       // add tag for rendering
-      Object.keys(csrPayload).length && ctx.tags.push({
+      Object.keys(payload).length && ctx.tags.push({
         tag: 'script',
-        innerHTML: JSON.stringify(csrPayload),
+        innerHTML: JSON.stringify(payload),
         props: { id: 'unhead:payload', type: 'application/json' },
       })
     },
