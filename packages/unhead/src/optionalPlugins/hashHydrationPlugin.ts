b8c852eb9172842148d771d1bb904fde7029f6f4
Harlan Wilton <harlan@harlanzw.com>
2023-09-10 12:39:03 +0200

    refactor: noop experimental hash hydration plugin

--- packages/unhead/src/optionalPlugins/hashHydrationPlugin.ts
+++ packages/unhead/src/optionalPlugins/hashHydrationPlugin.ts
@@ -1,43 +1,9 @@
-import { defineHeadPlugin, hashCode, hashTag } from '@unhead/shared'
-import type { Unhead } from '@unhead/schema'
+import { defineHeadPlugin } from '@unhead/shared'
+
+/**
+ * @deprecated Hash hydration is no longer supported. Please remove this plugin.
+ */
 
 /* @__NO_SIDE_EFFECTS__ */ export function HashHydrationPlugin() {
-  let prevHash: string | false = false
-  let dirty = false
-  let head: Unhead
-  return defineHeadPlugin({
-    hooks: {
-      'init': function (_head) {
-        head = _head
-        if (!head.ssr)
-          prevHash = head.resolvedOptions.document?.head.querySelector('meta[name="unhead:ssr"]')?.getAttribute('content') || false
-        if (!prevHash)
-          dirty = true
-      },
-      'tags:resolve': function ({ tags }) {
-        const nonServerTags = tags.filter(tag => tag._m !== 'server')
-
-        // always generate a hash
-        const hash = !nonServerTags.length
-          ? false
-          : hashCode(
-            nonServerTags
-              .map(tag => hashTag(tag))
-              .join(''),
-          )
-        // the SSR hash matches the CSR hash, we can skip the render
-        if (prevHash !== hash && prevHash !== false)
-          dirty = true
-        else
-          prevHash = hash
-      },
-      'dom:beforeRender': function (ctx) {
-        ctx.shouldRender = dirty
-        dirty = false
-      },
-      'ssr:render': function ({ tags }) {
-        prevHash && tags.push({ tag: 'meta', props: { name: 'unhead:ssr', content: String(prevHash) } })
-      },
-    },
-  })
+  return defineHeadPlugin({})
 }
