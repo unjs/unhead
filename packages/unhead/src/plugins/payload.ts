e4809d81ea3cebf9423841900aab11fbb2ac861c
Harlan Wilton <harlan@harlanzw.com>
2023-09-10 12:43:08 +0200

    chore: fix ts errors

--- packages/unhead/src/plugins/payload.ts
+++ packages/unhead/src/plugins/payload.ts
@@ -4,9 +4,10 @@ export default defineHeadPlugin({
   mode: 'server',
   hooks: {
     'tags:resolve': function (ctx) {
-      const payload: { titleTemplate: string | ((s: string) => string); templateParams: Record<string, string>; title: string } = {}
+      const payload: { titleTemplate?: string | ((s: string) => string); templateParams?: Record<string, string>; title?: string } = {}
       ctx.tags.filter(tag => ['titleTemplate', 'templateParams', 'title'].includes(tag.tag) && tag._m === 'server')
         .forEach((tag) => {
+          // @ts-expect-error untyped
           payload[tag.tag] = tag.tag.startsWith('title') ? tag.textContent : tag.props
         })
       // add tag for rendering
