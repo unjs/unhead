dbb5581193beed008bc4dbed1cccb5a9d51b8ac5
Harlan Wilton <harlan@harlanzw.com>
2023-11-22 02:11:42 +1100

    fix: check innerHTML is a string when applying XSS fixes
    Relates to https://github.com/nuxt/nuxt/issues/24388, https://github.com/nuxt-modules/fontaine/issues/350, https://github.com/unjs/unhead/issues/278

--- packages/unhead/src/plugins/xss.ts
+++ packages/unhead/src/plugins/xss.ts
@@ -4,7 +4,7 @@ export default defineHeadPlugin({
   hooks: {
     'tags:afterResolve': function (ctx) {
       for (const tag of ctx.tags) {
-        if (tag.innerHTML) {
+        if (typeof tag.innerHTML === 'string') {
           if (tag.innerHTML && ['application/ld+json', 'application/json'].includes(tag.props.type))
           // ensure </script> tags get encoded, this is only for JSON, it will break HTML if used
           { tag.innerHTML = tag.innerHTML.replace(/</g, '\\u003C') }
@@ -15,6 +15,7 @@ export default defineHeadPlugin({
               .replace(new RegExp(`</${tag.tag}`, 'g'), `<\\/${tag.tag}`)
           }
         }
+        // TODO delete innerHTML otherwise
       }
     },
   },
