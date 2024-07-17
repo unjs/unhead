e6b5d1aea01d2d8e7ad71793f7543e5a5a549c03
Negezor <negezor@gmail.com>
2024-07-18 02:47:10 +1100

    perf(unhead): find and remove once templateParams tag in templateParams plugin

--- packages/unhead/src/plugins/templateParams.ts
+++ packages/unhead/src/plugins/templateParams.ts
@@ -11,8 +11,20 @@ export default defineHeadPlugin(head => ({
   hooks: {
     'tags:resolve': (ctx) => {
       const { tags } = ctx
+      let templateParams: TemplateParams | undefined
+      for (let i = 0; i < tags.length; i += 1) {
+        const tag = tags[i]
+
+        if (tag.tag !== 'templateParams') {
+          continue
+        }
+
+        templateParams = ctx.tags.splice(i, 1)[0].props
+
+        i -= 1
+      }
       // we always process params so we can substitute the title
-      const params = (tags.find(tag => tag.tag === 'templateParams')?.props || {}) as TemplateParams
+      const params = (templateParams || {}) as TemplateParams
       // ensure a separator exists
       const sep = params.separator || '|'
       delete params.separator
@@ -45,7 +57,6 @@ export default defineHeadPlugin(head => ({
       // resolved template params
       head._templateParams = params
       head._separator = sep
-      ctx.tags = tags.filter(tag => tag.tag !== 'templateParams')
     },
   },
 }))
