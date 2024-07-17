cbae67e04b36a454b1eb2a7feea163fc36f9b918
Negezor <negezor@gmail.com>
2024-07-18 03:03:36 +1100

    perf(unhead): speed up payload plugin using one loop

--- packages/unhead/src/plugins/payload.ts
+++ packages/unhead/src/plugins/payload.ts
@@ -5,17 +5,29 @@ export default defineHeadPlugin({
   hooks: {
     'tags:resolve': (ctx) => {
       const payload: { titleTemplate?: string | ((s: string) => string), templateParams?: Record<string, string>, title?: string } = {}
-      ctx.tags.filter(tag => (tag.tag === 'titleTemplate' || tag.tag === 'templateParams' || tag.tag === 'title') && tag._m === 'server')
-        .forEach((tag) => {
-          // @ts-expect-error untyped
-          payload[tag.tag] = tag.tag.startsWith('title') ? tag.textContent : tag.props
+
+      let hasPayload = false
+
+      for (const tag of ctx.tags) {
+        if (tag._m !== 'server' || (tag.tag !== 'titleTemplate' && tag.tag !== 'templateParams' && tag.tag !== 'title')) {
+          continue
+        }
+
+        // @ts-expect-error untyped
+        payload[tag.tag] = tag.tag === 'title' || tag.tag === 'titleTemplate'
+          ? tag.textContent
+          : tag.props
+        hasPayload = true
+      }
+
+      if (hasPayload) {
+        // add tag for rendering
+        ctx.tags.push({
+          tag: 'script',
+          innerHTML: JSON.stringify(payload),
+          props: { id: 'unhead:payload', type: 'application/json' },
         })
-      // add tag for rendering
-      Object.keys(payload).length && ctx.tags.push({
-        tag: 'script',
-        innerHTML: JSON.stringify(payload),
-        props: { id: 'unhead:payload', type: 'application/json' },
-      })
+      }
     },
   },
 })
