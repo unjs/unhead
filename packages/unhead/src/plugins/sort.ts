a53e10f5ff064ca648f0627bb28962983438398a
Negezor <negezor@gmail.com>
2024-07-17 20:39:37 +1100

    perf(unhead): swap the loops in the sorting plugin

--- packages/unhead/src/plugins/sort.ts
+++ packages/unhead/src/plugins/sort.ts
@@ -3,21 +3,26 @@ import { SortModifiers, defineHeadPlugin, tagWeight } from '@unhead/shared'
 export default defineHeadPlugin({
   hooks: {
     'tags:resolve': (ctx) => {
-      const tagPositionForKey = (key: string) => ctx.tags.find(tag => tag._d === key)?._p
-
       // 2a. Sort based on priority
       // now we need to check render priority for each before: rule and use the dedupe key index
-      for (const { prefix, offset } of SortModifiers) {
-        for (const tag of ctx.tags) {
-          if (typeof tag.tagPriority !== 'string' || !tag.tagPriority!.startsWith(prefix)) {
+      for (const tag of ctx.tags) {
+        if (typeof tag.tagPriority !== 'string') {
+          continue
+        }
+
+        for (const { prefix, offset } of SortModifiers) {
+          if (!tag.tagPriority.startsWith(prefix)) {
             continue
           }
 
-          const position = tagPositionForKey(
-            (tag.tagPriority as string).substring(prefix.length),
-          )
-          if (position !== undefined)
+          const key = (tag.tagPriority as string).substring(prefix.length)
+
+          const position = ctx.tags.find(tag => tag._d === key)?._p
+
+          if (position !== undefined) {
             tag._p = position + offset
+            break
+          }
         }
       }
 
