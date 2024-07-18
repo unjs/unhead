9e4e2711440a9deb94c405f3986f80f5a4e1f53f
Negezor <negezor@gmail.com>
2024-07-18 16:08:37 +1100

    perf(ssr): use string concatenation in ssrRenderTags

--- packages/ssr/src/util/ssrRenderTags.ts
+++ packages/ssr/src/util/ssrRenderTags.ts
@@ -3,25 +3,29 @@ import { propsToString, tagToString } from '.'
 
 export function ssrRenderTags<T extends HeadTag>(tags: T[], options?: RenderSSRHeadOptions) {
   const schema: {
-    tags: Record<'head' | 'bodyClose' | 'bodyOpen', string[]>
+    tags: Record<'head' | 'bodyClose' | 'bodyOpen', string>
     htmlAttrs: HeadTag['props']
     bodyAttrs: HeadTag['props']
-  } = { htmlAttrs: {}, bodyAttrs: {}, tags: { head: [], bodyClose: [], bodyOpen: [] } }
+  } = { htmlAttrs: {}, bodyAttrs: {}, tags: { head: '', bodyClose: '', bodyOpen: '' } }
+
+  const lineBreaks = !options?.omitLineBreaks ? '\n' : ''
 
   for (const tag of tags) {
     if (tag.tag === 'htmlAttrs' || tag.tag === 'bodyAttrs') {
       schema[tag.tag] = { ...schema[tag.tag], ...tag.props }
       continue
     }
-    schema.tags[tag.tagPosition || 'head'].push(tagToString(tag))
+    const s = tagToString(tag)
+    const tagPosition = tag.tagPosition || 'head'
+    schema.tags[tagPosition] += schema.tags[tagPosition]
+      ? `${lineBreaks}${s}`
+      : s
   }
 
-  const lineBreaks = !options?.omitLineBreaks ? '\n' : ''
-
   return {
-    headTags: schema.tags.head.join(lineBreaks),
-    bodyTags: schema.tags.bodyClose.join(lineBreaks),
-    bodyTagsOpen: schema.tags.bodyOpen.join(lineBreaks),
+    headTags: schema.tags.head,
+    bodyTags: schema.tags.bodyClose,
+    bodyTagsOpen: schema.tags.bodyOpen,
     htmlAttrs: propsToString(schema.htmlAttrs),
     bodyAttrs: propsToString(schema.bodyAttrs),
   }
