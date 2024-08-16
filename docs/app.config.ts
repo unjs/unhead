3227e1ed0f42c24e65426e4a7cc619f7766030ab
harlan <harlan@harlanzw.com>
2024-08-16 16:08:52 +1000

    docs: maybe fix 500

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,9 +1,4 @@
 export default defineAppConfig({
-  site: {
-    name: 'Unhead',
-    description: 'Unhead is the any-framework document head manager built for performance and delightful developer experience.',
-    tagline: 'Get your &lt;head&gt; in the game.',
-  },
   ui: {
     primary: 'emerald',
     gray: 'slate',
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -25,6 +25,8 @@ export default defineNuxtConfig({
   site: {
     name: 'Unhead',
     url: 'unhead.unjs.io',
+    description: 'Unhead is the any-framework document head manager built for performance and delightful developer experience.',
+    tagline: 'Get your &lt;head&gt; in the game.',
   },
 
   runtimeConfig: {
