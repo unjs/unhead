86ac40457e6dd3dfd65c09150dab1ffd29c36e13
harlan <harlan@harlanzw.com>
2025-01-24 09:38:33 +1100

    chore: experimenting with vue streaming

---
+++ examples/vite-ssr-vue-streaming/src/pages/async-head.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useHead } from '@unhead/vue'
+import { onMounted, getCurrentInstance, withAsyncContext } from 'vue'
+
+// await new Promise((resolve) => setTimeout(resolve, 100))
+// useHead({
+//   title: 'post promise'
+// })
+const vm = getCurrentInstance()
+
+onMounted(async () => {
+  await new Promise((resolve) => setTimeout(resolve, 100))
+  setCurrentInstance(vm)
+  useHead({
+    title: 'onMounted post promise'
+  })
+})
+</script>
+
+<template>
+  <div>
+    <h1>async-head</h1>
+  </div>
+</template>
---
+++ examples/vite-ssr-vue-streaming/src/pages/inner-html-script.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useScript } from '@unhead/vue'
+
+const isScriptLoaded = ref(false)
+
+interface TestApi {
+  test: () => void
+}
+
+const { $script, test } = useScript<TestApi>({
+  key: 'test',
+  src: 'example.js',
+  onload() {
+    console.log('script loaded')
+    isScriptLoaded.value = true
+  },
+  onerror(e) {
+    console.log('script error', e)
+  },
+}, {
+  use() {
+    return window.myScript
+  },
+  transform: async (script) => {
+    delete script.src
+    script.innerHTML = await new Promise((resolve) => {
+      resolve('(function() { window.myScript = { test: () => console.log("hello world") } })()')
+    })
+    return script
+  },
+  trigger: 'manual',
+})
+test()
+$script.then(() => {
+  console.log('ready!')
+})
+
+useHead({
+  title: computed(() => $script.status.value),
+})
+</script>
+
+<template>
+  <div>
+    <h1>inner-html-script</h1>
+    <div>
+      script status: {{ $script.status }}
+    </div>
+    <button v-if="$script.status.value === 'awaitingLoad'" @click="$script.load">
+      Load Script
+    </button>
+    <button v-if="$script.status.value === 'loaded'" @click="$script.remove">
+      Remove Script
+    </button>
+  </div>
+</template>
+
+<style scoped>
+h1,
+a {
+  color: green;
+}
+</style>
