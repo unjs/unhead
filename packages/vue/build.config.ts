10a5cb454dd56c093fef46b2656aa7d37bc06206
harlan <harlan@harlanzw.com>
2025-02-06 01:19:34 +1100

    fix!: drop `CJS` exports

--- packages/vue/build.config.ts
+++ packages/vue/build.config.ts
@@ -3,9 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   clean: true,
   declaration: true,
-  rollup: {
-    emitCJS: true,
-  },
   externals: ['vue'],
   entries: [
     { input: 'src/index', name: 'index' },
