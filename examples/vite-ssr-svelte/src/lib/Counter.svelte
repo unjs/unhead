6303a14bce64e5f68eadf300faee8f59ea89ec4c
harlan <harlan@harlanzw.com>
2025-02-21 13:31:45 +1100

    chore: clean up

--- examples/vite-ssr-svelte/src/lib/Counter.svelte
+++ examples/vite-ssr-svelte/src/lib/Counter.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 import { useHead, useScript } from '@unhead/svelte'
-import {onMount} from "svelte";
 
 let count: number = $state(0)
 let isConfettiReady = $state(false)
@@ -21,22 +20,15 @@ $effect(() => {
   })
 })
 
-onMount(() => {
-  console.log('mounted')
-})
-
 const { onLoaded } = useScript('https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js', {
   use() {
     if (typeof window === 'undefined') {
-      console.log('bad use() on server')
       return
     }
-    console.log('good use() on client', !!(window as any).JSConfetti)
     return (window as any).JSConfetti
   }
 })
 onLoaded((inst) => {
-  console.log('loaded js confetti', inst)
   isConfettiReady = true
   jsConfetti = new inst()
 })
