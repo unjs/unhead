0cec152e153e47bf621368aa7a4566edbb78fd6f
harlan <harlan@harlanzw.com>
2025-03-06 01:12:43 +1100

    test: validate package exports

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   test: {
     pool: 'threads',
-    workspace: ['packages/*'],
+    workspace: ['packages/*', 'test/'],
     globals: true,
     reporters: 'dot',
     isolate: true,
