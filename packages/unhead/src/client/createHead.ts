6e1dff27a4494edf4f7e2b31f56b9fef1bcaae4c
harlan <harlan@harlanzw.com>
2025-03-07 17:32:20 +1100

    fix(unhead): `init` option should trigger DOM renders
    Fixes #513

--- packages/unhead/src/client/createHead.ts
+++ packages/unhead/src/client/createHead.ts
@@ -3,22 +3,24 @@ import { createUnhead } from '../unhead'
 import { renderDOMHead } from './renderDOMHead'
 
 export function createHead<T = ResolvableHead>(options: CreateClientHeadOptions = {}) {
-  // restore initial entry from payload (titleTemplate and templateParams)
-  const head = createUnhead<T>({
-    document: (typeof window !== 'undefined' ? document : undefined),
-    ...options,
-  })
-  // restore initial entry from payload (titleTemplate and templateParams)
-  const initialPayload = head.resolvedOptions.document?.head.querySelector('script[id="unhead:payload"]')?.innerHTML || false
-  if (initialPayload) {
-    head.push(JSON.parse(initialPayload))
-  }
   const render = options.domOptions?.render || renderDOMHead
-  head.use({
-    key: 'client',
-    hooks: {
-      'entries:updated': render,
-    },
+  options.document = options.document || (typeof window !== 'undefined' ? document : undefined)
+  const initialPayload = options.document?.head.querySelector('script[id="unhead:payload"]')?.innerHTML || false
+  // restore initial entry from payload (titleTemplate and templateParams)
+  return createUnhead<T>({
+    ...options,
+    plugins: [
+      ...(options.plugins || []),
+      {
+        key: 'client',
+        hooks: {
+          'entries:updated': render,
+        },
+      },
+    ],
+    init: [
+      JSON.parse(initialPayload || '{}'),
+      ...(options.init || []),
+    ],
   })
-  return head
 }
--- packages/unhead/test/unit/client/titleTemplate.test.ts
+++ packages/unhead/test/unit/client/titleTemplate.test.ts
@@ -3,13 +3,15 @@ import { createClientHeadWithContext, useDom } from '../../util'
 
 describe('titleTemplate', () => {
   it('string replace', async () => {
-    const head = createClientHeadWithContext()
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
     head.push({
       titleTemplate: '%s - my template',
       title: 'test',
     })
 
-    const dom = useDom()
     await renderDOMHead(head, { document: dom.window.document })
 
     expect(dom.window.document.title).toMatchInlineSnapshot(
@@ -17,112 +19,135 @@ describe('titleTemplate', () => {
     )
   })
   it('fn replace', async () => {
-    const head = createClientHeadWithContext()
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
     head.push({
       titleTemplate: (title?: string) => `${title} - my template`,
       title: 'test',
     })
-    const dom = useDom()
     await renderDOMHead(head, { document: dom.window.document })
     expect(dom.window.document.title).toMatchInlineSnapshot(
       `"test - my template"`,
     )
   })
   it('titleTemplate as title', async () => {
-    const head = createClientHeadWithContext()
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
     head.push({
       titleTemplate: (title?: string) => title ? `${title} - Template` : 'Default Title',
       title: null,
     })
-    const dom = useDom()
     await renderDOMHead(head, { document: dom.window.document })
     expect(dom.window.document.title).toMatchInlineSnapshot(
       `"Default Title"`,
     )
   })
   // TODO convert to client
-  // it('titleTemplate as title - update', async () => {
-  //   const head = createClientHeadWithContext()
-  //   head.push({
-  //     titleTemplate: (title?: string) => title ? `${title} - Template` : 'Default Title',
-  //   })
-  //   expect((await renderSSRHead(head)).headTags).toMatchInlineSnapshot(
-  //     `"<title>Default Title</title>"`,
-  //   )
-  //   const entry = head.push({
-  //     title: 'Hello world',
-  //   })
-  //   expect((await renderSSRHead(head)).headTags).toMatchInlineSnapshot(
-  //     `"<title>Hello world - Template</title>"`,
-  //   )
-  //   entry.dispose()
-  //   expect((await renderSSRHead(head)).headTags).toMatchInlineSnapshot(
-  //     `"<title>Default Title</title>"`,
-  //   )
-  // })
-  // it('reset title template', async () => {
-  //   const head = createClientHeadWithContext()
-  //   head.push({
-  //     titleTemplate: (title?: string) => title ? `${title} - Template` : 'Default Title',
-  //   })
-  //   head.push({
-  //     titleTemplate: null,
-  //     title: 'page title',
-  //   })
-  //   const { headTags } = await renderSSRHead(head)
-  //   expect(headTags).toMatchInlineSnapshot(
-  //     `"<title>page title</title>"`,
-  //   )
-  // })
-  //
-  // it('nested title template', async () => {
-  //   const head = createClientHeadWithContext()
-  //   head.push({
-  //     titleTemplate: (title?: string) => title ? `${title} - Template` : 'Default Title',
-  //   })
-  //   head.push({
-  //     titleTemplate: null,
-  //   })
-  //   const { headTags } = await renderSSRHead(head)
-  //   expect(headTags).toMatchInlineSnapshot(
-  //     '""',
-  //   )
-  // })
-  //
-  // it('null fn return', async () => {
-  //   const head = createClientHeadWithContext()
-  //   head.push({
-  //     titleTemplate: (title?: string) => title === 'test' ? null : `${title} - Template`,
-  //     title: 'test',
-  //   })
-  //   const { headTags } = await renderSSRHead(head)
-  //   expect(headTags).toMatchInlineSnapshot(
-  //     '""',
-  //   )
-  // })
-  //
-  // it('empty title', async () => {
-  //   const head = createClientHeadWithContext()
-  //   head.push({
-  //     title: '',
-  //   })
-  //   const { headTags } = await renderSSRHead(head)
-  //   expect(headTags).toMatchInlineSnapshot(
-  //     `""`,
-  //   )
-  // })
-  //
-  // it('replacing title with empty', async () => {
-  //   const head = createClientHeadWithContext()
-  //   head.push({
-  //     title: 'test',
-  //   })
-  //   head.push({
-  //     title: '',
-  //   })
-  //   const { headTags } = await renderSSRHead(head)
-  //   expect(headTags).toMatchInlineSnapshot(
-  //     `""`,
-  //   )
-  // })
+  it('titleTemplate as title - update', async () => {
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
+    head.push({
+      titleTemplate: (title?: string) => title ? `${title} - Template` : 'Default Title',
+    })
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`"Default Title"`)
+    const entry = head.push({
+      title: 'Hello world',
+    })
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`"Hello world - Template"`)
+    entry.dispose()
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`"Default Title"`)
+  })
+  it('reset title template', async () => {
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
+    head.push({
+      titleTemplate: (title?: string) => title ? `${title} - Template` : 'Default Title',
+    })
+    head.push({
+      titleTemplate: null,
+      title: 'page title',
+    })
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`"page title"`)
+  })
+
+  it('nested title template', async () => {
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
+    head.push({
+      titleTemplate: (title?: string) => title ? `${title} - Template` : 'Default Title',
+    })
+    head.push({
+      titleTemplate: null,
+    })
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`""`)
+  })
+
+  it('null fn return', async () => {
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
+    head.push({
+      titleTemplate: (title?: string) => title === 'test' ? null : `${title} - Template`,
+      title: 'test',
+    })
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`""`)
+  })
+
+  it('empty title', async () => {
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
+    head.push({
+      title: '',
+    })
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`""`)
+  })
+
+  it('replacing title with empty', async () => {
+    const dom = useDom()
+    const head = createClientHeadWithContext({
+      document: dom.window.document,
+    })
+    head.push({
+      title: 'test',
+    })
+    head.push({
+      title: '',
+    })
+    await renderDOMHead(head, { document: dom.window.document })
+    expect(dom.window.document.title).toMatchInlineSnapshot(`""`)
+  })
+  it('#513', async () => {
+    const dom = useDom()
+    createClientHeadWithContext({
+      document: dom.window.document,
+      init: [
+        { titleTemplate: title => `${title ?? ''} — Jetfly Group`.replace(/^ — /, '') },
+      ],
+    })
+
+    // wait
+    await new Promise(resolve => setTimeout(resolve, 100))
+
+    expect(dom.window.document.title).toMatchInlineSnapshot(`"Jetfly Group"`)
+  })
 })
