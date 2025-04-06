0b111d19a243565c617025344e663bdd049682cb
kricsleo <kricsleo@163.com>
2025-04-06 19:09:17 +0800

    fix(vue): use `setTimeout` as render's debounced delayer

--- packages/vue/src/client.ts
+++ packages/vue/src/client.ts
@@ -1,7 +1,6 @@
 import type { CreateClientHeadOptions } from 'unhead/types'
 import type { VueHeadClient } from './types'
 import { createHead as _createHead, createDebouncedFn, renderDOMHead } from 'unhead/client'
-import { nextTick } from 'vue'
 import { vueInstall } from './install'
 
 export { VueHeadMixin } from './VueHeadMixin'
@@ -10,7 +9,7 @@ export { renderDOMHead } from 'unhead/client'
 export function createHead(options: CreateClientHeadOptions = {}): VueHeadClient {
   const head = _createHead({
     domOptions: {
-      render: createDebouncedFn(() => renderDOMHead(head), nextTick),
+      render: createDebouncedFn(() => renderDOMHead(head), setTimeout),
     },
     ...options,
   }) as VueHeadClient
