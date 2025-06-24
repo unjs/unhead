a1c374f2f61508789645092d9930e87032c88250
Harlan Wilton <harlan@harlanzw.com>
2025-06-24 18:54:52 +1000

    chore: angular v20 compat

--- examples/angular/src/app/app.config.server.ts
+++ examples/angular/src/app/app.config.server.ts
@@ -1,14 +1,12 @@
 import {mergeApplicationConfig, ApplicationConfig} from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRoutesConfig } from '@angular/ssr';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 import { provideServerHead } from '@unhead/angular/server'
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes),
+    provideServerRendering(withRoutes(serverRoutes)),
     provideServerHead({
       init: [
         {
