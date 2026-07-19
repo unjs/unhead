357b0fa10c79d0482f95ffe3709c5ca5ebd31d52
Harlan Wilton <harlan@harlanzw.com>
2026-07-19 12:32:06 +1000

    fix(schema-org): export missing node types

--- packages/schema-org/test/public-types.test.ts
+++ packages/schema-org/test/public-types.test.ts
@@ -1,4 +1,19 @@
-import type { ResolverOptions } from '../src'
+import type {
+  DefinedRegion,
+  MerchantReturnPolicy,
+  MerchantReturnPolicySimple,
+  MonetaryAmount,
+  MonetaryAmountSimple,
+  OfferShippingDetails,
+  Place,
+  PlaceSimple,
+  QuantitativeSimple,
+  QuantitativeValue,
+  ResolverOptions,
+  ShippingDeliveryTime,
+  VirtualLocation,
+  VirtualLocationSimple,
+} from '../src'
 import { expectTypeOf, it } from 'vitest'
 
 it('exports schema resolver options from the package entry', () => {
@@ -6,4 +21,15 @@ it('exports schema resolver options from the package entry', () => {
   expectTypeOf<ResolverOptions>().toHaveProperty('root')
   expectTypeOf<ResolverOptions>().toHaveProperty('generateId')
   expectTypeOf<ResolverOptions>().toHaveProperty('afterResolve')
+})
+
+it('exports schema node types used by public helpers', () => {
+  expectTypeOf<DefinedRegion>().toHaveProperty('addressCountry')
+  expectTypeOf<MerchantReturnPolicy>().toEqualTypeOf<MerchantReturnPolicySimple>()
+  expectTypeOf<MonetaryAmount>().toEqualTypeOf<MonetaryAmountSimple>()
+  expectTypeOf<QuantitativeValue>().toEqualTypeOf<QuantitativeSimple>()
+  expectTypeOf<OfferShippingDetails>().toHaveProperty('shippingDestination')
+  expectTypeOf<Place>().toEqualTypeOf<PlaceSimple>()
+  expectTypeOf<ShippingDeliveryTime>().toHaveProperty('transitTime')
+  expectTypeOf<VirtualLocation>().toEqualTypeOf<VirtualLocationSimple>()
 })
